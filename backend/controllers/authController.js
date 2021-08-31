4ee61b0132f3ab668ceb83e0c474537f3c3ff378
danyy <dany@gmail.com>
2021-08-30 21:34:51 -0500

    API

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,7 +3,7 @@ const bcryptjs = require('bcryptjs');
 const { validationResult } = require('express-validator');
 const jwt = require('jsonwebtoken');
 
-exports.crearUsuario = async (req, res) => {
+exports.autenticarUsuario = async (req, res) => {
 
     //Revisar si hay errores
     const errores = validationResult(req);
@@ -11,30 +11,24 @@ exports.crearUsuario = async (req, res) => {
         return res.status(400).json({ errores: errores.array() })
     }
 
-    //Extraer email y password
+    //extraer el email y password
     const { email, password } = req.body;
-
 
     try {
 
-        //Revisar que el usuario sea único
+        //Revisar que sea un usuario registrado
         let usuario = await Usuario.findOne({ email });
-
-        if (usuario) {
-            return res.status(400).json({ msg: 'El usuario ya existe' })
+        if (!usuario) {
+            return res.status(400).json({ msj: 'El usuario no existe' })
         }
 
-        //crea el nuevo usuario
-        usuario = new Usuario(req.body);
+        //Revisar password
+        const passCorrecto = await bcryptjs.compare(password, usuario.password);
+        if (!passCorrecto) {
+            return res.status(400).json({ msj: 'Password Incorrecto' })
+        }
 
-        //Hashear password
-        const salt = await bcryptjs.genSalt(10);
-        usuario.password = await bcryptjs.hash(password, salt);
-
-
-        //guardar el nuevo usuario
-        await usuario.save();
-
+        //Si todo es correcto
         //Crear el JWT
         const payload = {
             usuario: {
@@ -53,10 +47,8 @@ exports.crearUsuario = async (req, res) => {
 
         });
 
-
     } catch (error) {
         console.log(error);
-        res.status(400).send('Hubo un error');
     }
 
 }
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,18 +1,17 @@
-//Rutas para crear usuarios
+//Rutas para autenticar usuarioa
 const express = require('express');
 const router = express.Router();
-const usuarioController = require('../controllers/usuarioController');
 const { check } = require('express-validator');
+const authController = require('../controllers/authController');
 
 //Crea un usuario
-//api/usuarios
+//api/auth
 router.post('/', 
     [
-        check('nombre', 'El nombre es obligatorio').not().isEmpty(),
         check('email', 'Agrega un email válido').isEmail(),
         check('password', 'EL password debe ser mínimo de 6 caracteres').isLength({min: 6 })
     ],
-    usuarioController.crearUsuario
+    authController.autenticarUsuario
 );
 
 module.exports = router;
