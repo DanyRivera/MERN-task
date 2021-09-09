a41ac646cf11113ad39d7ecb30f6572d715cebc9
Dany <dany@gmail.com>
2021-09-09 15:53:26 -0500

    xd

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,1 +1,12 @@
 const jwt = require('jsonwebtoken');
+
+module.exports = function(req, res, next) {
+    //Leer el token del header
+    const token = req.header('x-auth-token');
+
+    console.log(token);
+
+    //Revisar si no hay token
+
+    //validar el token
+}
