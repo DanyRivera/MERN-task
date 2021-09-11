dc3004863fdff4b18687ee9cb04d4adf6bb81b31
Dany <dany@gmail.com>
2021-09-10 20:24:44 -0500

    Terminando el backend

--- backend/controllers/tareaController.js
+++ backend/controllers/tareaController.js
@@ -11,4 +11,136 @@ exports.crearTarea = async (req, res) => {
         return res.status(400).json({ errores: errores.array() })
     }
 
+    try {
+
+        //Extraer el proyecto y comprobar si existe
+        const { proyecto } = req.body;
+
+        const existeProyecto = await Proyecto.findById(proyecto);
+        if (!existeProyecto) {
+            return res.status(404).json({ msg: 'Proyecto no encontrado' })
+        }
+
+        //revisar si el proyecto actual pertenece al usuario autenticado
+        if (existeProyecto.creador.toString() !== req.usuario.id) {
+            return res.status(401).json({ msg: 'No autorizado' })
+        }
+
+        //Creamos tarea
+        const tarea = new Tarea(req.body);
+        await tarea.save();
+        res.json({ tarea })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).send('Hubo un error')
+    }
+
+}
+
+
+//Obtiene las tareas por proyecto
+exports.obtenerTareas = async (req, res) => {
+
+    try {
+        //Extraer el proyecto y comprobar si existe
+        const { proyecto } = req.body;
+
+        const existeProyecto = await Proyecto.findById(proyecto);
+        if (!existeProyecto) {
+            return res.status(404).json({ msg: 'Proyecto no encontrado' })
+        }
+
+        //revisar si el proyecto actual pertenece al usuario autenticado
+        if (existeProyecto.creador.toString() !== req.usuario.id) {
+            return res.status(401).json({ msg: 'No autorizado' })
+        }
+
+        //obtener las tareas por proyecto
+        const tareas = await Tarea.find({ proyecto });
+        res.json({ tareas })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    }
+
+}
+
+//Actualizar una tarea
+exports.actualizarTarea = async (req, res) => {
+
+    try {
+
+        //Extraer el proyecto y comprobar si existe
+        const { proyecto, nombre, estado } = req.body;
+
+        //Si la tarea existe o no
+        let tarea = await Tarea.findById(req.params.id);
+
+        if (!tarea) {
+            return res.status(404).json({ msg: 'No existe esa tarea' })
+        }
+
+        const existeProyecto = await Proyecto.findById(proyecto);
+
+        //revisar si el proyecto actual pertenece al usuario autenticado
+        if (existeProyecto.creador.toString() !== req.usuario.id) {
+            return res.status(401).json({ msg: 'No autorizado' })
+        }
+
+        //Crear un objeto con la nueva informacion
+        const nuevaTarea = {};
+
+        if (nombre) {
+            nuevaTarea.nombre = nombre;
+        }
+
+        if (estado) {
+            nuevaTarea.estado = estado;
+        }
+
+        //Guardar la tarea
+        tarea = await Tarea.findOneAndUpdate({ _id: req.params.id }, nuevaTarea, { new: true });
+
+        res.json({ tarea })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    }
+
+}
+
+//Eliminar una tarea
+exports.eliminaTarea = async (req, res) => {
+
+    try {
+
+        //Extraer el proyecto y comprobar si existe
+        const { proyecto } = req.body;
+
+        //Si la tarea existe o no
+        let tarea = await Tarea.findById(req.params.id);
+
+        if (!tarea) {
+            return res.status(404).json({ msg: 'No existe esa tarea' })
+        }
+
+        const existeProyecto = await Proyecto.findById(proyecto);
+
+        //revisar si el proyecto actual pertenece al usuario autenticado
+        if (existeProyecto.creador.toString() !== req.usuario.id) {
+            return res.status(401).json({ msg: 'No autorizado' })
+        }
+
+        //Eliminar
+        await Tarea.findOneAndRemove({_id: req.params.id})
+        res.json({msg: 'Tarea Eliminada'})
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    }
+
 }
--- backend/routes/tareas.js
+++ backend/routes/tareas.js
@@ -9,10 +9,30 @@ const { check } = require('express-validator');
 router.post('/', 
     auth, 
     [
-        check('nombre', 'El nombre es obligatorio').not().isEmpty()
+        check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+        check('proyecto', 'El proyecto es obligatorio').not().isEmpty()
     ],
     tareaController.crearTarea
 
 );
+
+//Obetener las tareas por poryecto
+router.get('/', 
+    auth,
+    tareaController.obtenerTareas
+);
+
+//Actualizar tarea
+router.put('/:id', 
+    auth,
+    tareaController.actualizarTarea
+)
+
+//Elimina una tarea
+router.delete('/:id', 
+    auth,
+    tareaController.eliminaTarea
+)
+
 
 module.exports = router;
